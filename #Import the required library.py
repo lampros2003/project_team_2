9da1dc9c605a5757275f472ece4affaa5a87556d
Lampros Avouris <lamprosavouris@gmail.com>
2021-12-07 19:11:49 +0200

    τα πολύ βασικά της μπάρας είναι οκ

--- #Import the required library.py
+++ #Import the required library.py
@@ -4,10 +4,10 @@ win = Tk()
 win.geometry("750x250")
 
 my_variable = StringVar()
-def search(var, index, mode):
+def tracer(var, index, mode):
    print ("{}".format(my_variable.get()))
 
-my_variable.trace_variable("w", search)
+my_variable.trace_variable("w", tracer)
 Label(win, textvariable = my_variable).pack(padx=5, pady=5)
 Entry(win, textvariable = my_variable, width=20).pack(ipadx=20,padx=5, pady=5)
 win.mainloop()
--- searchbegin.py
+++ searchbegin.py
@@ -3,22 +3,40 @@ import tkinter as tk
 
 
 #Η κλαση μπαρα παιρνει ενα tk.Tk() object και το κανει παραθυρο αναζητησης
+#Η κλάση bara λειτουργεί με self structure(δες αν γίνεται να συμπεριλάβεις το παράθυρο μέσα στην μπάρα)
 class bara():
     
-    def __init__(self, w):
+    def __init__(self, w):#το initialization της μπάρας 
         self.w = w
         self.w.title("Αναζήτηση")
-        self.draw() 
         self.search()
+        self.draw() 
+        
     
-    def draw(self):
-        var = tk.StringVar()
+    def draw(self):#το γραφικό περιβάλλων,ν γίνει πιο ευπαρουσίαστο
+        global var
+        var = tk.StringVar(self.w)#το κείμενο μεσα στην μπαρα ορίζεται ως μεταβλητη var (οχι τελικό ονομα)
         self.lab1 = tk.Label(self.w, text = 'Γράψε δίπλα για αναζήτηση' )
         self.lab1.grid(row=1,column=0)
         self.ent = tk.Entry(self.w, textvariable= var)
         self.ent.grid(row=1,column=1)
-        
+        self.listbo = tk.Listbox(self.w,)
+        for i in searchreturn:
+            self.listbo.insert(searchreturn.index(i),i)
+        self.listbo.grid(row=1,column=2)
+        def displayvalue(*args):#εκτυπωνει τπ περιεχόμενο της μπαρας 
+    
+            print(args)
+            print(str(var.get()))
+
+        var.trace_add('write', displayvalue)#κανει trace το περιεχώμενο της μπαρας
     def search(self):
+        #εδω ψαχνει το database(μη ολοκληρωμένο)
+        global searchreturn
+        searchreturn = ['apo1','apo2','apo3','apo4']
+        
+        
+        
 
 
 w = tk.Tk()
