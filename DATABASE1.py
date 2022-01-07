2aae0a7455ca6145cf00bf37b4ff960749aa6f68
lampros2003 <46668710+lampros2003@users.noreply.github.com>
2022-01-08 00:23:52 +0200

    Bugfix στο internet part και αποθηκευση pdf στο δισκο
    Διωρθωσα ένα bug εκει που είχε ξεχάσει ο Δήμος να ορίσει το self.url
    προσθεσα μετρητη ως γνώρισμα της student (SOS να βάλουμε το νούμερο και στο database ) καθώς και εφτιαξα προγραμμα που αποθηκεύει τα pdf αν το ζητησει ο χρήστης (επισης προσορηνα εβγαλα το try except για να βλέπουμε τι λάθει βγάζει το πρόγραμμα)

--- DATABASE1.py
+++ DATABASE1.py
@@ -1,5 +1,5 @@
 import sqlite3
-from internet_part2 import *
+from internet_part import *
 from unidecode import unidecode
 
 amount = 0
--- internet_part.py
+++ internet_part.py
@@ -1,16 +1,28 @@
+from tkinter.constants import TRUE
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-
-
+import os
+download = True
+error = False
+localize_pdf = True
+save_path = '/pdfs'
+if not os.path.exists(save_path):
+    os.mkdir(save_path)
 def download_pdf(soup):
     links=soup.find_all('a',{'target':'_blank'})
     for link in links:
         if '.pdf' in link.get_text():
             url='https://nemertes.library.upatras.gr/'+link['href']
             return url
+def download_file(download_url, filename):
+    response = urlopen(download_url)    
+    file = open(filename + ".pdf", 'wb')
+    file.write(response.read())
+    file.close()
 class Student():
     count=0
-    def __init__(self,writer,title,trans_title,date,kwords,trans_kwords,summary):
+    def __init__(self, countofstu ,writer,title,trans_title,date,kwords,trans_kwords,summary , url):
+        self.countofstu = countofstu 
         self.writer=writer
         self.title=title
         self.trans_title=trans_title
@@ -18,14 +30,15 @@ class Student():
         self.kwords=kwords
         self.trans_kwords=trans_kwords
         self.summary=summary
-        self.url=url
+        self.url= url
         self.count+=1
+        
     def display_count(self):
         print(self.count)
     def return_contents(self):
         return self.writer + ' , '+self.title +' , '+self.trans_title +' , '+self.date + ' , '+self.kwords +' , '+self.trans_kwords +' , ' +self.summary+' , '+self.url
     def __repr__(self) :
-        return self.writer + ' , '+self.title +' , '+self.trans_title +' , '+self.date + ' , '+self.kwords +' , '+self.trans_kwords +' , ' +self.summary+' , '+self.url
+        return str(self.countofstu) + ' , ' +self.writer + ' , '+self.title +' , '+self.trans_title +' , '+self.date + ' , '+self.kwords +' , '+self.trans_kwords +' , ' +self.summary+' , '+self.url
     
 def secondary_pages(s):
     bases={}
@@ -39,6 +52,11 @@ def secondary_pages(s):
         bases[i]=(datalist[1].get_text(" "))
         i+=1
     bases[i]=download_pdf(soup)
+    if localize_pdf :
+        completeName = os.path.join(save_path, str(Student.count))
+        print(str(Student.count))
+        print('at work')
+        download_file(bases[list(bases)[-1]],completeName)
     return bases
 
 def main_pages():
@@ -47,28 +65,36 @@ def main_pages():
     x = []
 
     
-    try:
-        while True:
-            url=urlopen('https://nemertes.library.upatras.gr/jspui/handle/10889/43?offset={}'.format(count))
-            info=url.read()
-            soup=BeautifulSoup(info,'html.parser')
-            table=soup.find('table',{'class':'table'})
-            i=0
-            for tag in table.find_all('tr'):
-                if i==0:
-                    i+=1
-                    continue
-                datalist=list(tag.children)
-                for link in datalist[1].find_all('a', href=True):
-                    s='https://nemertes.library.upatras.gr'+link['href']
-                    pfile=secondary_pages(s)
-                    x.append(Student(pfile[2],pfile[0],pfile[1],datalist[0].get_text(),pfile[3], pfile[4],pfile[6],pfile[7]))
-            count+=20
+    #try:
+    while True:
+        url=urlopen('https://nemertes.library.upatras.gr/jspui/handle/10889/43?offset={}'.format(count))
+        info=url.read()
+        soup=BeautifulSoup(info,'html.parser')
+        table=soup.find('table',{'class':'table'})
+        i=0
+        for tag in table.find_all('tr'):
+            if i==0:
+                i+=1
+                continue
+            datalist=list(tag.children)
+            for link in datalist[1].find_all('a', href=True):
+                s='https://nemertes.library.upatras.gr'+link['href']
+                pfile=secondary_pages(s)
+
+                x.append(Student(Student.count,pfile[2],pfile[0],pfile[1],datalist[0].get_text(),pfile[3], pfile[4],pfile[6],pfile[7]))
+                Student.count += 1
+                print(x)
+        count+=20
+        if count == 40:
+            print(x[-1])
+            break    
             
             
-            
-    except:
-        print("Completed")
+    #except:
+        
+        #print(count)
+        #global error
+        #error = True
         
 
-if __name__ =="__main__":main_pages()
+if download :main_pages()
