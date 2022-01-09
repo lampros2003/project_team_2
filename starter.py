3731d09797d6d482c14ddf4dab6f9b573cc913c0
panos243 <47045045+panos243@users.noreply.github.com>
2022-01-09 12:44:30 +0200

    arxiko interface

--- starter.py
+++ starter.py
@@ -1,22 +1,19 @@
 import tkinter as tk
 
+download = False
 
 def yes():
     global download
     download = True
     page.destroy()
 
-def no():
-    global download
-    download = False
-    page.destroy()
     
 page = tk.Tk()
 page.resizable(False,False)
 
 b1 = tk.Button(page,text ='renew database',font ='Arial 30',bg='blue',command = yes)
 b1.pack()
-b2 = tk.Button(page,text ='offline version',font ='Arial 30',bg='blue',command = no)
+b2 = tk.Button(page,text ='offline version',font ='Arial 30',bg='blue',command = page.destroy)
 b2.pack()
 
 page.mainloop()
