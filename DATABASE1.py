6ee7933b91066701b776aad3128a77d397433796
panos243 <47045045+panos243@users.noreply.github.com>
2022-01-11 23:58:03 +0200

    FINISHED!!

--- DATABASE1.py
+++ DATABASE1.py
@@ -4,7 +4,6 @@ from bs4 import element
 from Internet_part3 import *
 from unidecode import unidecode
 
-amount = 0
 
 con=sqlite3.connect('work.db') #database που απηθηκεύουμε σε αρχειο
 c = con.cursor()
@@ -12,6 +11,7 @@ c = con.cursor()
 if download and not error:
     try:
         c.execute('DROP TABLE work;')#διαγραφει την database εαν υπαρχει 
+        con.commit()
     except:
         pass
     
@@ -32,20 +32,15 @@ try:
 
 
     for i in range(len(x)):
-        c.execute("INSERT INTO work VALUES (?,?,?,?,?,?,?,?,?,?)",(x[i].stucount,unidecode(x[i].writer),unidecode(x[i].title),x[i].trans_title,x[i].date,unidecode(x[i].kwords),x[i].trans_kwords,x[i].summary,x[i].trans_summary,x[i].url))#είσαγουμε τα στοιχεια στο database
+        c.execute("INSERT INTO work VALUES (?,?,?,?,?,?,?,?,?,?)",(x[i].stucount,unidecode(x[i].writer),unidecode(x[i].title),x[i].trans_title,x[i].date,unidecode(x[i].kwords),x[i].trans_kwords,unidecode(x[i].summary),x[i].trans_summary,x[i].url))#είσαγουμε τα στοιχεια στο database
         
 except:
     pass
 
 con.commit()
 
-def elements():
-        allofthem = c.execute("SELECT COUNT(name) FROM work").fetchone()
-        string = str(allofthem[0])+" elements availabe"#κοιταει ποσα στοιχεια εχει
-        return string
 
 def search(z):
-        global amount
         z = unidecode(z)
         result = c.execute("SELECT number,name,trans_title,date,url FROM work WHERE name LIKE '%{}%'COLLATE NOCASE ".format(z)).fetchall()
         result = result + c.execute("SELECT number,name,trans_title,date,url FROM work WHERE (title LIKE '%{}%' or trans_title LIKE'%{}%') and not name LIKE '%{}%' COLLATE NOCASE".format(z,z,z)).fetchall()
@@ -53,35 +48,27 @@ def search(z):
         result = result + c.execute("SELECT number,name,trans_title,date,url FROM work WHERE summary LIKE '%{}%' and not trans_kwords LIKE'%{}%' and not kwords LIKE'%{}%' and not name LIKE '%{}%' and not title LIKE '%{}%' and not trans_title LIKE '%{}%' COLLATE NOCASE".format(z,z,z,z,z,z)).fetchall() 
         result = result + c.execute("SELECT number,name,trans_title,date,url FROM work WHERE date Like '%{}%' and not summary LIKE '%{}%' and not trans_kwords LIKE'%{}%' and not kwords LIKE'%{}%' and not name LIKE '%{}%' and not title LIKE '%{}%' and not trans_title LIKE '%{}%' COLLATE NOCASE".format(z,z,z,z,z,z,z)).fetchall()
         con.commit()
-        amount = len(result)
         return result
 
 
 def searchbydate(z):
-        global amount
         z = unicode(z)
         result = c.execute("SELECT number,name,trans_title,date,url FROM work WHERE name LIKE '%{}%' or trans_title LIKE'%{}%' or date LIKE'%{}%' or trans_kwords LIKE'%{}%' or title LIKE'%{}%' or kwords LIKE'%{}%' or summary LIKE '%{}%' COLLATE NOCASE ".format(z,z,z,z,z,z,z)).fetchall()
         con.commit()
-        amount = len(result)
         return result
 
 
 def searchname(z):
-        global amount
         z = unidecode(z)
         result = c.execute("SELECT number,name,trans_title,date,url FROM work WHERE name LIKE '%{}%' COLLATE NOCASE".format(z)).fetchall()
         con.commit()
-        amount = len(result)
         return result
 
         
 def searchtitle(z):
-        global amount
         z = unidecode(z)
         result = c.execute("SELECT number,name,trans_title,date,url FROM work WHERE trans_title LIKE '%{}%' or title LIKE '%{}%' COLLATE NOCASE".format(z,z)).fetchall()
         con.commit()
-        amount = len(result)
         return result
     
 
-print(elements())
