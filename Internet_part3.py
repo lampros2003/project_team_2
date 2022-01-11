0a346f6d9d99724e5aeac8866326dd44560bb17c
panos243 <47045045+panos243@users.noreply.github.com>
2022-01-11 23:52:47 +0200

    σαν καινουριο

--- Internet_part3.py
+++ Internet_part3.py
@@ -94,10 +94,9 @@ def main_pages():
             if counts%100==0:
                 print('{:3.2f}%'.format((len(x)/2288)*100))
             counts+=20
-            if counts == 40 :
-                break
-    except :
+    except AttributeError :
         print("Completed")
+    except:
         global error
         error = True
         
