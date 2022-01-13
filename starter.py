0fff52f5096c942d46feba4ac3043cc6b6e7274e
panos243 <47045045+panos243@users.noreply.github.com>
2022-01-13 16:54:00 +0200

    namechange

--- starter.py
+++ starter.py
@@ -8,13 +8,15 @@ global save_path
 
 download = False
 localize_pdf = False
+
 if not os.path.isfile('wheretosave.txt') :
     with open('wheretosave.txt', 'w') as f:
         f.write(str('nemertespdfs'))
 else :
     with open('wheretosave.txt', 'r') as f:
         save_path = f.read()
-def check():
+     
+def check():   #ελέγχει εάν υπάρχει το database. εάν οχι δίνει warning
     checking = os.path.isfile("work.db")
     print(save_path)
     if not checking:
@@ -23,7 +25,7 @@ def check():
         check = False
     elif internet: page.destroy()
     
-def done():
+def yes():
     global localize_pdf
     answer = messagebox.askquestion("Προσοχη!","Για να κατεβούν τα pdf χρειάζεται πολύ ώρα.Είστε σίγουροι οτι θέλετε να συνεχίσετε;")
     if answer == 'yes':
@@ -33,17 +35,16 @@ def done():
             f.write(save_path)
         pdfpage.destroy()
 
-def yes():
+def renew():
     global download,check
     download,check = True,True
-    
     page.destroy()
 
 if internet:   
     page = tk.Tk()
     page.resizable(False,False)
 
-    b1 = tk.Button(page,text ='renew database',font ='Arial 25',bg='blue',command = yes)
+    b1 = tk.Button(page,text ='renew database',font ='Arial 25',bg='blue',command = renew)
     b1.pack(fill = 'x')
     b2 = tk.Button(page,text ='offline version',font ='Arial 25',bg='blue',command = check)
     b2.pack(fill = 'x')
@@ -58,7 +59,7 @@ if download:
     
     l1 = tk.Label(pdfpage,text = "Do you want to download the pdf's?",font ='Arial 20',bg='white')
     l1.pack(fill = 'x')
-    b1 = tk.Button(pdfpage,text ="YES",font ='Arial 25',bg='blue',command = done)
+    b1 = tk.Button(pdfpage,text ="YES",font ='Arial 25',bg='blue',command = yes)
     b1.pack(fill = 'x')
     b2 = tk.Button(pdfpage,text ='NO',font ='Arial 25',bg='blue',command = pdfpage.destroy)
     b2.pack(fill = 'x')
