2bfd0dccb39bdfde7a0388e6e1103fe2618e4f12
panos243 <47045045+panos243@users.noreply.github.com>
2022-01-14 19:41:10 +0200

    greektitle

--- DATABASE1.py
+++ DATABASE1.py
@@ -19,6 +19,7 @@ if download:
         number text,
         Greekname text,
         name text,
+        Greektitle text,
         title text,
         trans_title text,
         date text,
@@ -32,7 +33,7 @@ if download:
 
 
     for i in range(len(x)):   #είσαγουμε τα στοιχεια απο την x στο table
-        c.execute("INSERT INTO work VALUES (?,?,?,?,?,?,?,?,?,?,?)",(x[i].stucount,x[i].writer,unidecode(x[i].writer),unidecode(x[i].title),x[i].trans_title,x[i].date,unidecode(x[i].kwords),x[i].trans_kwords,unidecode(x[i].summary),x[i].trans_summary,x[i].url))
+        c.execute("INSERT INTO work VALUES (?,?,?,?,?,?,?,?,?,?,?,?)",(x[i].stucount,x[i].writer,unidecode(x[i].writer),x[i].title,unidecode(x[i].title),x[i].trans_title,x[i].date,unidecode(x[i].kwords),x[i].trans_kwords,unidecode(x[i].summary),x[i].trans_summary,x[i].url))
     con.commit()
 
 # η συνάρτηση search δίνει λίστα με το νούμερο,όνομα,τίτλο,ημερομηνία και url των στοιχείων που έχουν το z 
@@ -40,21 +41,21 @@ if download:
 def search(z):  
         z = unidecode(z)
         result = c.execute("SELECT number,Greekname,trans_title,date,url FROM work WHERE name LIKE '%{}%'COLLATE NOCASE ".format(z)).fetchall()
-        result = result + c.execute("SELECT number,Greekname,trans_title,date,url FROM work WHERE (title LIKE '%{}%' or trans_title LIKE'%{}%') and not name LIKE '%{}%' COLLATE NOCASE".format(z,z,z)).fetchall()
-        result = result + c.execute("SELECT number,Greekname,trans_title,date,url FROM work WHERE (trans_kwords LIKE'%{}%' or  kwords LIKE'%{}%') and not name LIKE '%{}%' and not title LIKE '%{}%' and not trans_title LIKE '%{}%' COLLATE NOCASE".format(z,z,z,z,z)).fetchall()
-        result = result + c.execute("SELECT number,Greekname,trans_title,date,url FROM work WHERE summary LIKE '%{}%' and not trans_kwords LIKE'%{}%' and not kwords LIKE'%{}%' and not name LIKE '%{}%' and not title LIKE '%{}%' and not trans_title LIKE '%{}%' COLLATE NOCASE".format(z,z,z,z,z,z)).fetchall() 
-        result = result + c.execute("SELECT number,Greekname,trans_title,date,url FROM work WHERE date Like '%{}%' and not summary LIKE '%{}%' and not trans_kwords LIKE'%{}%' and not kwords LIKE'%{}%' and not name LIKE '%{}%' and not title LIKE '%{}%' and not trans_title LIKE '%{}%' COLLATE NOCASE".format(z,z,z,z,z,z,z)).fetchall()
+        result = result + c.execute("SELECT number,Greekname,Greektitle,date,url FROM work WHERE (title LIKE '%{}%' or trans_title LIKE'%{}%') and not name LIKE '%{}%' COLLATE NOCASE".format(z,z,z)).fetchall()
+        result = result + c.execute("SELECT number,Greekname,Greektitle,date,url FROM work WHERE (trans_kwords LIKE'%{}%' or  kwords LIKE'%{}%') and not name LIKE '%{}%' and not title LIKE '%{}%' and not trans_title LIKE '%{}%' COLLATE NOCASE".format(z,z,z,z,z)).fetchall()
+        result = result + c.execute("SELECT number,Greekname,Greektitle,date,url FROM work WHERE summary LIKE '%{}%' and not trans_kwords LIKE'%{}%' and not kwords LIKE'%{}%' and not name LIKE '%{}%' and not title LIKE '%{}%' and not trans_title LIKE '%{}%' COLLATE NOCASE".format(z,z,z,z,z,z)).fetchall() 
+        result = result + c.execute("SELECT number,Greekname,Greektitle,date,url FROM work WHERE date Like '%{}%' and not summary LIKE '%{}%' and not trans_kwords LIKE'%{}%' and not kwords LIKE'%{}%' and not name LIKE '%{}%' and not title LIKE '%{}%' and not trans_title LIKE '%{}%' COLLATE NOCASE".format(z,z,z,z,z,z,z)).fetchall()
         return result
 
 
 def searchname(z):      # δίνει λίστα με το νούμερο,όνομα,τίτλο,ημερομηνία και url των στοιχείων που έχουν το z κάπου στον όνομα
         z = unidecode(z)
-        result = c.execute("SELECT number,Greekname,trans_title,date,url FROM work WHERE name LIKE '%{}%' COLLATE NOCASE".format(z)).fetchall()
+        result = c.execute("SELECT number,Greekname,Greektitle,date,url FROM work WHERE name LIKE '%{}%' COLLATE NOCASE".format(z)).fetchall()
         return result
 
         
 def searchtitle(z):   # δίνει λίστα με το νούμερο,όνομα,τίτλο,ημερομηνία και url των στοιχείων που έχουν το z κάπου στον τίτλο
         z = unidecode(z)
-        result = c.execute("SELECT number,Greekname,trans_title,date,url FROM work WHERE trans_title LIKE '%{}%' or title LIKE '%{}%' COLLATE NOCASE".format(z,z)).fetchall()
+        result = c.execute("SELECT number,Greekname,Greektitle,date,url FROM work WHERE trans_title LIKE '%{}%' or title LIKE '%{}%' COLLATE NOCASE".format(z,z)).fetchall()
         return result
    
